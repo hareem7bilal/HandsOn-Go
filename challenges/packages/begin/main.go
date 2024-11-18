28d088238f5522bd69f78a61af90be712469536f
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-11-18 07:41:58 +0000

    challenge 1

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -1,10 +1,18 @@
-// challenges/packages/begin/proverbs.go
 package main
 
-// import the proverbs package
+import (
+	"fmt"
+
+	// import the proverbs package
+	"github.com/jboursiquot/go-proverbs"
+)
 
 // getRandomProverb returns a random proverb from the proverbs package
+func getRandomProverb() *proverbs.Proverb {
+	return proverbs.Random()
+}
 
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Printf("%+v\n", getRandomProverb())
 }
