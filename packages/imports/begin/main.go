b885876db34839bafe97cd6e0869dc769fcba569
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-11-18 07:19:17 +0000

    imports

--- packages/imports/begin/main.go
+++ packages/imports/begin/main.go
@@ -2,11 +2,17 @@
 package main
 
 // import the fmt package from the standard library
+import "fmt"
+
 
 // import the time package from the standard library
+import "time"
+
 
 func main() {
 	// use the fmt package to print the string "Hello Gopher!"
+	fmt.Println("Konnichiwa Gopher San!")
 
 	// use the time package to print the current weekday
+	fmt.Printf("Kyou wa %s desu\n", time.Now().Weekday())
 }
